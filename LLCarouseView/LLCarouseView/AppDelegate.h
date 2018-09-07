97deaa17bc085a265dcc7ee93de73a199cfd7434
YoloHwang <huangyongle@YoloHwangdeMacBook-Pro.local>
2018-09-07 11:09:50 +0800

    init project

---
+++ LLCarouseView/LLCarouseView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLCarouseView
+//
+//  Created by YoloHwang on 2018/9/6.
+//  Copyright © 2018年 Yolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
